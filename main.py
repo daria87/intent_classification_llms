7c2740ddf847e78ca62940ef4565bd1ee06dd07a
daria87 <daria.alexander87@gmail.com>
2024-12-11 15:22:52 +0100

    Import chain of thought

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from read_file import read_tsv
 from config import api_key
 from intent_classification import QueryIntentClassifier
-from prompts import DEF_ONLY, DEF_KW, DEF_KW_FEW_SHOT
+from prompts import DEF_ONLY, DEF_KW, DEF_KW_FEW_SHOT, CHAIN_OF_THOUGHT
 
 if __name__ == "__main__":
     orcas_gold = read_tsv("input/ORCAS-I-gold_1.tsv")
-    orcas_gold = orcas_gold[:10]
     api_url = "https://api.deepinfra.com/v1/inference/meta-llama/Meta-Llama-3-70B-Instruct"
     classifier = QueryIntentClassifier(api_key, api_url)
     orcas_gold = classifier.apply_to_df(orcas_gold,'llama70B_level1',DEF_KW_FEW_SHOT)
