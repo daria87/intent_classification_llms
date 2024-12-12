022e11dd66fd8ed3a51047e3913b5298fcc2c6d2
daria87 <daria.alexander87@gmail.com>
2024-12-12 10:31:37 +0100

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -7,3 +7,11 @@ def read_tsv(filepath, **kwargs):
     except Exception as e:
         print(f"Error reading the TSV file: {e}")
         return None
+
+def write_tsv(df, file_path, encoding='utf-8', index=False):
+    try:
+        # Write the DataFrame to a TSV file
+        df.to_csv(file_path, sep='\t', encoding=encoding, index=index)
+        print(f"Data written to {file_path}")
+    except Exception as e:
+        print(f"Error writing to the file: {e}")
