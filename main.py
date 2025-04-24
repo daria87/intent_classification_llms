b3df4c8eb1e86109936b4480be53f88054248077
daria87 <daria.alexander87@gmail.com>
2025-04-24 12:08:00 +0200

    change prompt name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from read_file import read_tsv
 from config import api_key
 from intent_classification import QueryIntentClassifier
-from prompts_level1_cat import DEF_ONLY_L1, DEF_KW_L1, DEF_KW_FEW_SHOT_L1, CHAIN_OF_THOUGHT_L1
+from prompts_level1_cat import DEF_ONLY_L1, DEF_KW_L1, DEF_KW_FEW_SHOT_L1, CLUE_AND_REAS_L1
 from prompts_all_cat import DEF_ONLY_ALL, DEF_KW_ALL, DEF_KW_FEW_SHOT_ALL, CHAIN_OF_THOUGHT_ALL
 
 if __name__ == "__main__":
