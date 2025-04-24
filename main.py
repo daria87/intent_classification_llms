6c3ea465cdbad96a4d31cefb397408fd597e2900
daria87 <daria.alexander87@gmail.com>
2025-04-24 12:12:08 +0200

    change prompt name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from read_file import read_tsv
 from config import api_key
 from intent_classification import QueryIntentClassifier
 from prompts_level1_cat import DEF_ONLY_L1, DEF_KW_L1, DEF_KW_FEW_SHOT_L1, CLUE_AND_REAS_L1
-from prompts_all_cat import DEF_ONLY_ALL, DEF_KW_ALL, DEF_KW_FEW_SHOT_ALL, CHAIN_OF_THOUGHT_ALL
+from prompts_all_cat import DEF_ONLY_ALL, DEF_KW_ALL, DEF_KW_FEW_SHOT_ALL
 
 if __name__ == "__main__":
     orcas_gold = read_tsv("input/ORCAS-I-gold_1.tsv")
